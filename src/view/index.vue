b6e5dfcba79e5c4939ab941a6d213a1d881330c5
钟永豪 <zhy6421867@qq.com>
2018-03-25 16:02:22 +0800

    index
    index layout

--- src/view/index.vue
+++ src/view/index.vue
@@ -2,23 +2,26 @@
 <template src="./index.html"></template>
 
 <script>
-  import bCarousel from 'bootstrap-vue/es/components/carousel/carousel';
   import bImg from 'bootstrap-vue/es/components/image/img';
 
   import mixins from '../mixins/index';
 
-  import Header from '../component/header/index';
-  import Footer from '../component/footer/index';
-
   export default {
     components: {
-      bCarousel,
-      bImg,
-      Header,
-      Footer
+      bImg
     },
     mixins: [mixins],
-    data: () => ({}),
+    data () {
+      return {
+        logo: require('../assets/img/logo.png'),
+        btn01: require('../assets/img/btn01.png'),
+        btn02: require('../assets/img/btn02.png'),
+        btn03: require('../assets/img/btn03.png'),
+        btn04: require('../assets/img/btn04.png'),
+        btn05: require('../assets/img/btn05.png'),
+        btn06: require('../assets/img/btn06.png')
+      }
+    },
     created() {
 
     },
